92dd5f985ce76d46f7f9e2def61dc7f8c4ecfab1
Soen Surya Soenaryo <wei.surya@gmail.com>
2019-12-01 10:09:37 +0700

    Fill TODO - faust

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,16 +33,16 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("org.cta.stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("org.cta.stations.table.v1", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   "org.cta.stations.table.v1",
+   default=TransformedStation,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
 #
@@ -52,7 +52,26 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
 #
 #
+@app.agent(topic)
+async def process(stream):
+    async for event in stream:
+        line = None
 
+        if event.red is True:
+            line = "red"
+        elif event.blue is True:
+            line = "blue"
+        elif event.green is True:
+            line = "green"
+        else:
+            logger.warning("Unknown line of %s", event)
+
+        table[event.station_id] = TransformedStation(
+            station_id=event.station_id,
+            station_name=event.station_name,
+            order=event.order,
+            line=line,
+        )
 
 if __name__ == "__main__":
     app.main()
